63bc84c471af11935af771b8d0ae4b7a97bd0370
nicolevenachi <ludwing@unicauca.edu.co>
2024-06-19 15:22:19 -0500

    01-Ts -> 05_Getters

--- 01_TSIntro/src/01-bases/03-classes.ts
+++ 01_TSIntro/src/01-bases/03-classes.ts
@@ -1,27 +1,13 @@
 
-export class Pokemon {
-
-  //declarar propiedades de clase
-  public id: number;
-  public name: string;
-  
-  // inicializar propiedades de clase
-  constructor(id: number, name: string){
-    this.id = id;
-    this.name = name;
-    
-    console.log('Object initialized with the data provided at the class instantiation');
-    
-  }
-}
-
+// declaaracion explicita
 // export class Pokemon {
 
+//   //declarar propiedades de clase
+//   public id: number;
+//   public name: string;
   
 //   // inicializar propiedades de clase
-//   constructor(
-//     public readonly id: number, 
-//     public name: string){
+//   constructor(id: number, name: string){
 //     this.id = id;
 //     this.name = name;
     
@@ -30,7 +16,43 @@ export class Pokemon {
 //   }
 // }
 
-//instnciar clase
-export const charmander = new Pokemon(1, 'charmander') 
+// declaaracion implicita
+export class Pokemon {
 
-charmander.id = 2
+  
+  // inicializar propiedades de clase
+  constructor(
+    public readonly id: number, 
+    public name: string,
+  ){
+    this.id = id;
+    this.name = name;
+    
+    console.log('Object initialized with the data provided at the class instantiation');
+    
+  }
+
+  //getters
+  get imageUrl(): string {
+    return `https://pokemon.com/${this.id}.jpg`
+  }
+
+  //metodos de clase
+  scream() {
+    console.log(`${this.name.toLocaleUpperCase()} !!!`);
+
+    charmander.speak();
+  }
+  private speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
+}
+
+//instnciar clase
+export const charmander = new Pokemon(1, 'charmander', 
+) 
+
+// charmander.id = 2
+console.log(charmander.imageUrl);
+charmander.scream();
+
