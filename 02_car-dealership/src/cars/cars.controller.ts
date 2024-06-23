93037bf136478809ca4453dc865bf0fa19dd0a84
nicolevenachi <ludwing@unicauca.edu.co>
2024-06-22 20:19:19 -0500

    02-NestBuildingBlocks -> 04_ExceptionFilters; Get, Post, Patch, Delete

--- 02_car-dealership/src/cars/cars.controller.ts
+++ 02_car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -19,5 +19,24 @@ export class CarsController {
   getCarById( @Param('id', ParseIntPipe) id: number) {
     return this.cars.findOneById(Number(id))
   }
+
+  @Post()
+  createCar(@Body() body:any) {
+    return body
+  }
+
+  @Patch('/:id')
+  updateCar(@Body() body:any, @Param('id') id:string) {
+    return {body, id}
+  }
+  @Delete('/:id')
+  deleteCar(@Param('id') id:string) {
+    return {
+      method: 'DELETE',
+      id
+    }
+  }
 }
+
+
 
